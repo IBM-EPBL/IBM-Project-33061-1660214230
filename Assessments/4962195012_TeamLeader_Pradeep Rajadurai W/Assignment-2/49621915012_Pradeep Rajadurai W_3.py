be96a7f1bf8c061a0ee95972f1f51232da8c632c
pradeeprajaduraiw <113977815+pradeeprajaduraiw@users.noreply.github.com>
2022-10-21 12:04:19 +0530

    Delete Assessments directory

--- Assessments/4962195012_TeamLeader_Pradeep Rajadurai W/Assignment-2/49621915012_Pradeep Rajadurai W_3.py
+++
@@ -1,7 +0,0 @@
-s1="thatchaini" 
-s2="muthulakshmi" 
-print("String-1 :  ",s1)
-print("String-2 :  ",s2) 
-print("Concatenate\t: ",s1+s2) 
-print("String reverse\t: ",s1[::-1])
-print("String Slice\t: ",s2[2:4])
